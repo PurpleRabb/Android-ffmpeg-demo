258f2ff0798f966c1db4d8d79d025687c2297d81
liushuo <364725259@qq.com>
2020-08-29 10:32:48 +0800

    添加audio初始化1

--- app/src/main/cpp/AudioChannel.cpp
+++ app/src/main/cpp/AudioChannel.cpp
@@ -4,6 +4,16 @@
 
 #include "AudioChannel.h"
 
+void *audioinit(void *argv) {
+    AudioChannel *audioChannel = static_cast<AudioChannel *>(argv);
+    audioChannel->audio_init();
+}
+
+void *audiodecode(void *argv) {
+    AudioChannel *audioChannel = static_cast<AudioChannel *>(argv);
+    audioChannel->audio_decode();
+}
+
 AudioChannel::AudioChannel(int id, CallJavaHelper *javaHelper, AVCodecContext *codecContext)
         : BaseChannel(id, javaHelper, codecContext) {
     pkt_queue.setReleaseCallback(releasePacket);
@@ -18,8 +28,37 @@ void AudioChannel::play() {
     pkt_queue.setWork(1);
     frame_queue.setWork(1);
     isPlaying = true;
+    pthread_create(&pid_audioinit, nullptr,audioinit,this);
+    pthread_create(&pid_decode, nullptr,audiodecode,this);
 }
 
 void AudioChannel::stop() {
     isPlaying = false;
+}
+
+void AudioChannel::audio_init() {
+
+    SLEngineItf engineInterface = nullptr; //音频引擎
+    SLObjectItf engineObject = nullptr;  //音频对象
+    SLObjectItf outputMixObject = nullptr; //混音器
+    SLObjectItf bqPlayerObject = nullptr; //播放器
+    SLPlayItf bqPlayerInterface = nullptr; //回调接口
+    SLAndroidSimpleBufferQueueItf bqPlayerBufferQueue = nullptr;
+
+    SLresult result;
+    //初始化播放引擎====>
+    result = slCreateEngine(&engineObject, 0, nullptr,0, nullptr, nullptr);
+    if (result != SL_RESULT_SUCCESS)
+        return;
+    result = (*engineObject)->Realize(engineObject,SL_BOOLEAN_FALSE);
+    if (result != SL_RESULT_SUCCESS)
+        return;
+    result = (*engineObject)->GetInterface(engineObject,SL_IID_ENGINE,&engineInterface);
+    if (result != SL_RESULT_SUCCESS)
+        return;
+    //<=======初始化播放引擎
+}
+
+void AudioChannel::audio_decode() {
+
 }
--- app/src/main/cpp/AudioChannel.h
+++ app/src/main/cpp/AudioChannel.h
@@ -5,12 +5,24 @@
 #ifndef FFMPEGDEMO_AUDIOCHANNEL_H
 #define FFMPEGDEMO_AUDIOCHANNEL_H
 
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+
 #include "BaseChannel.h"
+
 class AudioChannel : public BaseChannel {
 public:
     AudioChannel(int id, CallJavaHelper *javaHelper, AVCodecContext *codecContext);
     ~AudioChannel();
     void play();
     void stop();
+
+    void audio_init();
+
+    void audio_decode();
+
+private:
+    pthread_t pid_audioinit;
+    pthread_t pid_decode;
 };
 #endif //FFMPEGDEMO_AUDIOCHANNEL_H
